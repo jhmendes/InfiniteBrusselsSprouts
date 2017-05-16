585edb15bc1aba73ecae27a344d28998f4080ea5
John Mendes <johnhenrymendes@gmail.comgit config --global user.name John>
2017-05-16 07:17:16 -0400

    took out some comments

--- public/js/sprouts.js
+++ public/js/sprouts.js
@@ -7,8 +7,7 @@ $('.more-sprouts').on('click', (event) => {
   let request = $.ajax({
     method: 'GET',
     url: '/tweets.json?page=' + page
-    // how would you know to put this here?
-  })
+  });
 
   request.done((tweets) => {
     tweets.forEach((tweet) => {
@@ -18,9 +17,8 @@ $('.more-sprouts').on('click', (event) => {
             <div class="body">${tweet.text}</div>
             <div class="user">${tweet.username}</div>
           </li>`);
-          // if its a string literal, it will append like shown? instead of one line?
 
-    })
-  })
+    });
+  });
   page++;
 });
