3a4cbcad45e3f502c2d3d8dd5dd2b9fad5633af7
John Mendes <johnhenrymendes@gmail.comgit config --global user.name John>
2017-05-16 07:56:38 -0400

    Added scroll feature

--- public/js/sprouts.js
+++ public/js/sprouts.js
@@ -1,9 +1,35 @@
 // YOUR CODE GOES HERE
-  let page = 2;
+// CLICK VERSION:
 
-$('.more-sprouts').on('click', (event) => {
-  event.preventDefault();
+//   let page = 2;
+//
+// $('.more-sprouts').on('click', (event) => {
+//   event.preventDefault();
+//
+//   let request = $.ajax({
+//     method: 'GET',
+//     url: '/tweets.json?page=' + page
+//   });
+//
+//   request.done((tweets) => {
+//     tweets.forEach((tweet) => {
+//
+//       $('.tweets').append(
+//         `<li class="tweet">
+//             <div class="body">${tweet.text}</div>
+//             <div class="user">${tweet.username}</div>
+//           </li>`);
+//
+//     });
+//   });
+//   page++;
+// });
 
+// SCROLL VERSION:
+
+let page = 2;
+
+$(window).scroll(function() {
   let request = $.ajax({
     method: 'GET',
     url: '/tweets.json?page=' + page
